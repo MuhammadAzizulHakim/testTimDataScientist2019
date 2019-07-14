47d33bc4c77440a96af7bf0c5d99ad32f06e0d9d
MuhammadAzizulHakim <19azizul92@gmail.com>
2019-07-14 07:32:54 +0700

    Done stratified sampling. Used just 10% of total datasets. Done distribution curve analysis for variable importance.
    Start split to train test. Next: Do Random Forest method.

--- lendingClubLoanCreditScoringModel.R
+++ lendingClubLoanCreditScoringModel.R
@@ -29,20 +29,47 @@ barplot(table(loan$is_bad), col = 'lightblue')
 
 #Cek perilaku variabel numerik untuk good vs bad loans:
 numeric_cols <- sapply(loan, is.numeric)
+
+#Mulai dari sini, split data dahulu menjadi jumlah observasi yang lebih sedikit, dikarenakan keterbatasan hardware
+#Teknik splitting data menggunakan stratified random samplin, untuk memaintain rasio jumlah data agar tetap representatif dengan jumlah data yang sebenarnya
+library(caret)
+library(knitr)
+#Create function to calculate percent distribution for factors
+pct <- function(x){
+  tbl <- table(x)
+  tbl_pct <- cbind(tbl,round(prop.table(tbl)*100,2))
+  colnames(tbl_pct) <- c('Count','Percentage')
+  kable(tbl_pct)
+}
+
+#Lakukan stratified random sampling, gunakan 10% data yang ada
+set.seed(42)
+remaining_data <- createDataPartition(y=loan$is_bad, p = .9, list = FALSE)
+str(remaining_data)
+#Split data menjadi stratum yang akan digunakan untuk analisis vs stratum yang tidak digunakan
+remaining_loan_data <- loan[remaining_data,]
+loan2 <- loan[-remaining_data,] #Ke depannya, kita hanya akan menggunakan data loan2
+#Test, apakah stratum loan2 sudah representatif dengan data loan
+pct(loan$is_bad)
+pct(loan2$is_bad)
+
 # turn the data into long format
 library(reshape2)
 #Beri nama kolom id observasi
 #melt(loan, id.vars="")
-loan_long <- melt(loan[,numeric_cols], id="is_bad")
+loan_long <- melt(loan2[,numeric_cols], id="is_bad")
 
-# plot the distribution for 'bad' and 'good' for each numeric variable
+#Plot the distribution for 'bad' and 'good' for each numeric variable
 library(ggplot2)
 p <- ggplot(aes(x = value, group = is_bad, colour = factor(is_bad)), 
             data = loan_long)
-
-# create the plot to check if there are any good variables that can be used in predictive models
+#Create the plot to check if there are any good variables that can be used in predictive models
+#Catatan: metode ini crash ketika menggunakan stratum 20% dari total dataset, apalagi ketika menggunakan keseluruhan dataset
 p + geom_density() +
   facet_wrap(~variable, scales="free")
+#Dari metode di atas, diperoleh variabel-variabel yang mempengaruhi status pinjaman, sebagai berikut:
+
+
 
 
 #Show the data setelah disimpulkan bahwa annual_inc dan int_rate sebagai dua predictor paling berpengaruh:
